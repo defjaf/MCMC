5f3bc502f68b4a9051b83a0a92fac4eaba446998
Andrew H. Jaffe <a.h.jaffe@gmail.com>
2006-06-06 09:35:02 +0000

    driver

---
+++ BeamFit/driver.py
@@ -0,0 +1,26 @@
+import matplotlib
+matplotlib.use('Agg')
+import pylab
+
+import cPickle
+
+import MAXIPOLBeamData as MP
+
+def main():
+    
+    dets = [13, 14, 15, 23, 24, 25, 33, 34, 35, 43, 44, 45]
+    
+    res = {}
+    for cols in [(4,5), (6,7)]:
+        res[cols] = MP.testTOI(nMC=(3000, 100000), useNormalizedBeam=True,
+               noCorrelations=True, fac=None, doBlock=True, cols=None, dets=dets,
+               mapOnly=False, nhits=None, neg=False, rangeScale=None)
+               
+    fp = open("all.pickle", 'wb')  
+    
+    cPickle.dump(res, fp)   
+    
+    fp.close()
+
+if __name__ == '__main__':
+    main()
