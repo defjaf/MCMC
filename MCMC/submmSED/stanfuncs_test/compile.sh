e25d43d84ca82702881736072f46841790e1f2f4
Andrew H. Jaffe <a.h.jaffe@gmail.com>
2017-10-13 10:41:54 +0100

    clean up compile.sh
    should make into Makefile!

--- MCMC/submmSED/stanfuncs_test/compile.sh
+++ MCMC/submmSED/stanfuncs_test/compile.sh
@@ -1,9 +1,12 @@
+CXX=clang++
+
 PYSTAN=/Library/Frameworks/Python.framework/Versions/2.7/lib/python2.7/site-packages/pystan
-clang++ -dynamiclib -o blackbody.dylib  -I/Users/jaffe/home/dev/stan/stan/src/ \
--I${PYSTAN}/stan/lib/stan_math/lib/boost_1.64.0 \
--I${PYSTAN}/stan/lib/stan_math/lib/eigen_3.3.3/ \
--I${PYSTAN}/stan/lib/stan_math \
--I${PYSTAN}/stan/lib/stan_math/lib/cvodes_2.9.0/include \
+STAN_MATH=${PYSTAN}/stan/lib/stan_math
+
+${CXX} -dynamiclib -o blackbody.dylib \
+-I/Users/jaffe/home/dev/stan/stan/src/ \
+-I${STAN_MATH} -I${STAN_MATH}/lib/boost_1.64.0 -I${STAN_MATH}/lib/eigen_3.3.3/ -I${STAN_MATH}/lib/cvodes_2.9.0/include \
 ./blackbody.cpp
 
 # -I/Library/Frameworks/Python.framework/Versions/2.7/include/python2.7 \
+#
