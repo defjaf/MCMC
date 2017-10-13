81d0d33e696a13e2dcafe843728c31eabc7f163c
Andrew H. Jaffe <a.h.jaffe@gmail.com>
2017-10-13 16:25:26 +0100

    more

--- MCMC/submmSED/stanfuncs_test/compile.sh
+++ MCMC/submmSED/stanfuncs_test/compile.sh
@@ -3,7 +3,7 @@ CXX=clang++
 PYSTAN=/Library/Frameworks/Python.framework/Versions/2.7/lib/python2.7/site-packages/pystan
 STAN_MATH=${PYSTAN}/stan/lib/stan_math
 
-INC="-I/Users/jaffe/home/dev/stan/stan/src/ \
+INC="-I${PYSTAN}/stan/src \
 -I${STAN_MATH} -I${STAN_MATH}/lib/boost_1.64.0 -I${STAN_MATH}/lib/eigen_3.3.3/ -I${STAN_MATH}/lib/cvodes_2.9.0/include"
 
 LDFLAGS=-dynamiclib
