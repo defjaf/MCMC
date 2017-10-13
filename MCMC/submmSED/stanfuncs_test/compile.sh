49b3700f21299f2743a0ccb1cf1c28b9b28c7d53
Andrew H. Jaffe <a.h.jaffe@gmail.com>
2017-10-13 10:56:06 +0100

    add makefile and cleanup compile.sh (no longer needed)

--- MCMC/submmSED/stanfuncs_test/compile.sh
+++ MCMC/submmSED/stanfuncs_test/compile.sh
@@ -3,10 +3,12 @@ CXX=clang++
 PYSTAN=/Library/Frameworks/Python.framework/Versions/2.7/lib/python2.7/site-packages/pystan
 STAN_MATH=${PYSTAN}/stan/lib/stan_math
 
-${CXX} -dynamiclib -o blackbody.dylib \
--I/Users/jaffe/home/dev/stan/stan/src/ \
--I${STAN_MATH} -I${STAN_MATH}/lib/boost_1.64.0 -I${STAN_MATH}/lib/eigen_3.3.3/ -I${STAN_MATH}/lib/cvodes_2.9.0/include \
-./blackbody.cpp
+INC="-I/Users/jaffe/home/dev/stan/stan/src/ \
+-I${STAN_MATH} -I${STAN_MATH}/lib/boost_1.64.0 -I${STAN_MATH}/lib/eigen_3.3.3/ -I${STAN_MATH}/lib/cvodes_2.9.0/include"
+
+LDFLAGS=-dynamiclib
+
+${CXX} ${LDFLAGS} -o blackbody.dylib ${INC} ./blackbody.cpp
 
 # -I/Library/Frameworks/Python.framework/Versions/2.7/include/python2.7 \
 #
