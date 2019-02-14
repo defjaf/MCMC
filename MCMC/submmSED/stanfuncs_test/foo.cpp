3d6a7a17eab5f64dd5a7b7ebe7790a3e35da4d32
Andrew H. Jaffe <a.h.jaffe@gmail.com>
2019-02-14 10:17:49 +0000

    standfunc comments

--- MCMC/submmSED/stanfuncs_test/foo.cpp
+++ MCMC/submmSED/stanfuncs_test/foo.cpp
@@ -1,1 +1,8 @@
 #include "foo.hpp"
+
+
+// extern "C"
+// Eigen::Matrix<double, Eigen::Dynamic,1>
+// foo(const Eigen::Matrix<double, Eigen::Dynamic,1>& x, std::ostream* pstream__ = 0){
+//   return foo<double>(x, pstream__);
+// }
